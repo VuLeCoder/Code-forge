75ea8a516a0179ee31698a39fa8e96a934cfecfb
VuLeCoder <tle63917@gmail.com>
2026-09-18 14:35:48 +0700

    fix: change cookie-parser import to CommonJS syntax

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
+import cookieParser = require('cookie-parser');
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
